cc43df4cff96fc8128beed206037174bd6892277
Louie Cabral <louiealdrin25@gmail.com>
2021-11-07 15:28:40 +0800

    About design done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,90 @@
-function About(){
-    return(
-        <h1>TETE</h1>
-    )
+function About() {
+  return (
+    <div>
+      <div className="about-container">
+        <div className="comp-about">
+          <h1>About Parallax Web Services</h1>
+          <div className="betlog">
+            <p>
+              Started as a small company, Parallax Web Services have since grown
+              into a large business, and making a name for itself in the Web
+              Development Industry. Since 2020 Parallax Web Services have
+              deployed 2,024 Website Projects and continuing to grow to this
+              day.{" "}
+            </p>
+            <p>
+              We have gotten better, stronger and wiser. Join us in our path to
+              success. Let’s Soar Together!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="comp-teams">
+        <h1>Team Making Parallax Web Services Possible</h1>
+        <div className="main">
+        <div className="emp-131">
+            <div className="emp-pic"></div>
+            <p>President/CEO</p>
+          </div>
+        </div>
+        <div className="teams">
+        <div className="emp-1">
+            <div className="emp-pic"></div>
+            <p>Project Manager</p>
+          </div>
+
+          <div className="emp-2">
+            <div className="emp-pic"></div>
+            <p>Project Manager</p>
+          </div>
+          <div className="emp-3">
+            <div className="emp-pic"></div>
+            <p>Project Manager</p>
+          </div>
+
+          <div className="emp-4">
+            <div className="emp-pic"></div>
+            <p>Senior Front End Developer</p>
+          </div>
+          <div className="emp-5">
+            <div className="emp-pic"></div>
+            <p>Junior Front End Developer</p>
+          </div>
+          <div className="emp-6">
+            <div className="emp-pic"></div>
+            <p>Junior Front End Developer</p>
+          </div>
+
+          <div className="emp-7">
+            <div className="emp-pic"></div>
+            <p>Senior Back End Developer</p>
+          </div>
+          <div className="emp-8">
+            <div className="emp-pic"></div>
+            <p>Junior Front End Developer</p>
+          </div>
+          <div className="emp-9">
+            <div className="emp-pic"></div>
+            <p>Junior Front End Developer</p>
+          </div>
+
+          <div className="emp-10">
+            <div className="emp-pic"></div>
+            <p>Graphic Designer</p>
+          </div>
+          <div className="emp-11">
+            <div className="emp-pic"></div>
+            <p>Graphic Designer</p>
+          </div>
+          <div className="emp-12">
+            <div className="emp-pic"></div>
+            <p>Graphic Designer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
