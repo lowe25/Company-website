007dcd5ab4d8cae3e72328663f7e98ac92a79eb8
lowe25 <louiealdrin25@gmail.com>
2022-07-03 20:28:59 +0800

    Form Validation Added on Contact Form

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,16 @@
-
+import {useState} from "react";
 function Contacts(){
+  const [req, setReq] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
+  const [msg, setMsg] = useState("");
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  if(name.length === 0 || email.length === 0 || contact.length === 0 || msg.length === 0){
+    setReq(true);
+  }
+  }
     return(
         <div>
         <div className="contacts-container">
@@ -15,20 +26,24 @@ function Contacts(){
           </div>
   
           <div className="form">
-              <form className="contact-form">
+              <form className="contact-form" onSubmit={handleSubmit}>
               <label>Name</label>
-              <input type="text" placeholder="Name" className="txt-name"/>
+              {req && name.length <= 0 ? (<p className="error"><b>Name Field Is Required</b></p>) : ("")}
+              <input type="text" placeholder="Name" className="txt-name" onChange={(e) => setName(e.target.value)}/>
   
               <label>Email</label>
-              <input type="email" placeholder="Name" className="txt-email"/>
+              {req && email.length <= 0 ? (<p className="error"><b>Email Field Is Required</b></p>) : ("")}
+              <input type="email" placeholder="Name" className="txt-email" onChange={(e) => setEmail(e.target.value)}/>
   
               <label>Contact</label>
-              <input type="text" placeholder="Contact" className="txt-contact"/>
+              {req && contact.length <= 0 ? (<p className="error"><b>Contact Field Is Required</b></p>) : ("")}
+              <input type="text" placeholder="Contact" className="txt-contact" onChange={(e) => setContact(e.target.value)}/>
   
               <label>Message</label>
-              <textarea placeholder="Enter Message" className="txt-area"></textarea>
+              {req && msg.length <= 0 ? (<p className="error"><b>Message Field Is Required</b></p>) : ("")}
+              <textarea placeholder="Enter Message" className="txt-area" onChange={(e) => setMsg(e.target.value)}></textarea>
               <label></label>
-              <input type="button" value="Submit" className="btn-submit"/>
+              <button type="submit" className="btn-submit">Submit</button>
               </form>
               <div className="contact-info">
                   <p>Contact Information</p>
